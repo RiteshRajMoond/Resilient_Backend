4c882de546de11ccb80a799b86bdea8eab7391df
Ritesh Raj Moond <moondritesh@gmail.com>
2025-03-26 14:09:52 +0530

    added ip-adress based throttling

---
+++ middleware/ip-throttler.js
@@ -0,0 +1,10 @@
+// @desc will be used to throttle the requests
+const slowDown = require('express-slow-down');
+
+const throttle = slowDown({
+    windowMs: 15*60*1000, // 15 mins
+    delayAfter: 50, // allow 50 requests per 15 mins, then...
+    delayMs: 500
+})
+
+module.export = throttle;
